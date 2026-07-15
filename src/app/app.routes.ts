6de5b02a869d373db17ef7d22df041850032dc66
Fatso <mawambafarai1@gmail.com>
2026-07-15 19:38:23 +0200

    feat: add registration page and rutes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { WatchlistComponent } from './components/watchlist/watchlist.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: 'watchlist', component: WatchlistComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
